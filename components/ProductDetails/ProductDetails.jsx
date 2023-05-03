0837d206556a3a150ca8ed2b97d706608e234528
LironKiloma <87006580+LironKiloma@users.noreply.github.com>
2023-05-03 10:57:07 +0300

    Changed a staticPage to serverSideProps

---
+++ components/ProductDetails/ProductDetails.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+import styles from './ProductDetails.module.css';
+
+export default function ProductDetails({
+  title, image, price, category, description,
+}) {
+  return (
+    <div className={styles.productContainer}>
+      <h1 className={styles.productTitle}>{title}</h1>
+      <div className={styles.imageContainer}>
+        <Image fill alt="product image" src={image} />
+      </div>
+      <span className={styles.category}>{category}</span>
+      <p className={styles.description}>{description}</p>
+      <div className={styles.priceContainer}>
+        {price}
+        <span className={styles.currency}>$</span>
+      </div>
+      <button type="button" className={styles.button}>
+        <Link href="/products">Back to Products Page</Link>
+      </button>
+
+    </div>
+  );
+}
+
+ProductDetails.defaultProps = {
+  title: '',
+  image: '',
+  price: 100,
+  category: '',
+  description: '',
+};
+
+ProductDetails.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  price: PropTypes.string,
+  category: PropTypes.string,
+  description: PropTypes.string,
+};
--- pages/products/[id].jsx
+++ pages/products/[id].jsx
@@ -1,71 +1,51 @@
-import Link from 'next/link';
 import React from 'react';
-import Image from 'next/image';
 import PropTypes from 'prop-types';
 import styles from './id.module.css';
+import ProductDetails from '../../components/ProductDetails/ProductDetails';
 
-export default function Product({
-  title, image, price, category, description,
-}) {
+export default function ProductPage({ productData }) {
   return (
     <div className={styles.productPageContainer}>
-      <div className={styles.productContainer}>
-        <h1 className={styles.productTitle}>{title}</h1>
-        <div className={styles.imageContainer}>
-          <Image fill alt="product image" src={image} />
-        </div>
-        <span className={styles.category}>{category}</span>
-        <p className={styles.description}>{description}</p>
-        <div className={styles.priceContainer}>
-          {price}
-          <span className={styles.currency}>$</span>
-        </div>
-        <button type="button" className={styles.button}>
-          <Link href="/products">Back to Products Page</Link>
-        </button>
-
-      </div>
+      <ProductDetails
+        title={productData.title}
+        image={productData.image}
+        price={productData.price}
+        category={productData.category}
+        description={productData.description}
+      />
     </div>
   );
 }
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
   const { id } = context.params;
 
   const res = await fetch(`https://fakestoreapi.com/products/${id}`);
   const productData = await res.json();
+  console.log('productData', productData);
   return {
     props: {
-      ...productData,
+      productData,
     },
   };
 }
 
-export async function getStaticPaths() {
-  const res = await fetch('https://fakestoreapi.com/products');
-  const productsArray = await res.json();
-  const idsArray = productsArray.map((product) => product.id);
-  console.log('idsArry', idsArray);
-  return {
-    paths: idsArray.map((id) => ({
-      params: { id: String(id) },
-    })),
-    fallback: false,
-  };
-}
-
-Product.defaultProps = {
-  title: '',
-  image: '',
-  price: 100,
-  category: '',
-  description: '',
+ProductPage.defaultProps = {
+  productData: {},
 };
 
-Product.propTypes = {
-  title: PropTypes.string,
-  image: PropTypes.string,
-  price: PropTypes.string,
-  category: PropTypes.string,
-  description: PropTypes.string,
+ProductPage.propTypes = {
+  productData: PropTypes.instanceOf(Object),
 };
+
+// export async function getStaticPaths() {
+//   const res = await fetch('https://fakestoreapi.com/products');
+//   const productsArray = await res.json();
+//   const idsArray = productsArray.map((product) => product.id);
+//   return {
+//     paths: idsArray.map((id) => ({
+//       params: { id: String(id) },
+//     })),
+//     fallback: false,
+//   };
+// }
